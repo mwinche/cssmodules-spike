ebd00bd9b0d0f66a13cb2457bf2f660f515afc91
Matt Winchester <m_winche@yahoo.com>
2015-09-16 16:11:20 -0600

    basic widgets are present, need to strip down styles now, then theme, then theme again

--- DropDown/DropDown.js
+++ DropDown/DropDown.js
@@ -1,5 +1,116 @@
 'use strict';
 
+import styles from './DropDown.css';
+
 export default class DropDown {
-  
+  constructor(element){
+    this.options = formatOptions(JSON.parse(element.getAttribute('options') || []));
+    this.label = element.getAttribute('label') || 'Select';
+    this.open = false;
+
+    let optionsHTML = this.options.reduce(
+      (htmlString, option) => htmlString + `
+        <li class="${styles['option']}" value="${option.value}">
+          ${option.name}
+        </li>
+      `
+    , '');
+
+    element.innerHTML = `
+      <div class="${styles['button']}">
+        ${this.label}
+      </div>
+      <ul class="${styles['optionsContainer']}">
+        ${optionsHTML}
+      </ul>
+    `;
+
+    this.optionsContainer = element.querySelector(`.${styles['optionsContainer']}`);
+    this.trigger = element.querySelector(`.${styles['button']}`);
+
+    let optionElements = this.optionsContainer.querySelectorAll(`.${styles['option']}`);
+
+    attachOptionData(optionElements, this.options);
+    attachListeners(optionElements, 'click', event => {
+      this.trigger.innerHTML = event.target.option.name;
+      this.markSelected(event.target);
+      this.toggleOpen();
+    });
+
+    this.trigger.addEventListener('click', event => {
+      this.toggleOpen();
+    });
+  }
+
+  toggleOpen(){
+    this.open = !this.open;
+
+    if(this.open){
+      addClass(this.optionsContainer, styles['optionsContainer--open']);
+      addClass(this.trigger, styles['button--open']);
+    }
+    else{
+      removeClass(this.optionsContainer, styles['optionsContainer--open']);
+      removeClass(this.trigger, styles['button--open']);
+    }
+  }
+
+  markSelected(element){
+    let selected = this.optionsContainer.querySelector(`.${styles['option--selected']}`);
+
+    if(selected){
+      removeClass(selected, styles['option--selected']);
+    }
+
+    addClass(element, styles['option--selected']);
+  }
 };
+
+function formatOptions(options){
+  return options.map(option => {
+    if(option === undefined || option === null){
+      return {
+        name: 'null',
+        value: 'null'
+      }
+    }
+
+    let name = option.name || option;
+    let value = option.value || option;
+
+    return { name, value };
+  });
+}
+
+function attachOptionData(elements, options){
+  for(var i = 0; i < elements.length; i++){
+    elements[i].option = options[i];
+  }
+}
+
+function attachListeners(nodeList, type, callback){
+  for(var i = 0; i < nodeList.length; i++){
+    let node = nodeList[i];
+    node.addEventListener(type, callback);
+  }
+}
+
+function removeClass(element, className){
+  let classes = element.className.split(' ');
+
+  let index = classes.indexOf(className);
+
+  if(index >= 0){
+    classes.splice(index, 1);
+  }
+
+  element.className = classes.join(' ');
+}
+
+function addClass(element, className){
+  let classes = element.className.split(' ');
+
+  classes.push(className);
+
+  element.className = classes.join(' ');
+}
--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 import DropDown from './DropDown/DropDown.js';
 import Button from './Button/Button.js';
 import TextArea from './TextArea/TextArea.js';
-import UpdateStatus from './UpdateStatus/UpdateStatus.js';
 import styles from './styles.css';
+import { AllHtmlEntities as Entities } from 'html-entities';
+
+let entities = new Entities();
 
 let ELEMENT = document.body.ELEMENT_NODE;
 let widgetConstructors = {
   DROPDOWN: DropDown,
   WFBUTTON: Button,
-  WFTEXTAREA: TextArea,
-  UPDATESTATUS: UpdateStatus
+  WFTEXTAREA: TextArea
 };
+
+let options = ['Complete', 'In Progress', 'Planning', 'Scheduled'];
 
 document.body.innerHTML = `
   <div class="${styles['middleAlign']}">
     <wfbutton label="Hiya son!"></wfbutton>
     <wftextarea></wftextarea>
+    <dropdown label="Status..." options="${entities.encode(JSON.stringify(options))}"></dropdown>
   </div>
 `;
 
